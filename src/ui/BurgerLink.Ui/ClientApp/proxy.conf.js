543adf3b311419de33597a8b9df9b4a9d48175cb
Shawn Neckelmann <shawn.neckelmann@nvoicepay.com>
2023-10-14 21:51:01 -0500

    wip

--- src/ui/BurgerLink.Ui/ClientApp/proxy.conf.js
+++ src/ui/BurgerLink.Ui/ClientApp/proxy.conf.js
@@ -1,7 +1,10 @@
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:54977';
+const target = env.ASPNETCORE_HTTPS_PORT ?
+  `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+  env.ASPNETCORE_URLS ?
+    env.ASPNETCORE_URLS.split(';')[0] :
+    'http://localhost:54977';
 
 const PROXY_CONFIG = [
   {
