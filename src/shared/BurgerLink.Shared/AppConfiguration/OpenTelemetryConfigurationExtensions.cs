b6c6d06248cd4fb29e8881532224a0470b8d1038
Shawn Neckelmann <shawn.neckelmann@nvoicepay.com>
2023-10-17 15:51:17 -0500

    wip

--- src/shared/BurgerLink.Shared/AppConfiguration/OpenTelemetryConfigurationExtensions.cs
+++ src/shared/BurgerLink.Shared/AppConfiguration/OpenTelemetryConfigurationExtensions.cs
@@ -1,23 +1,34 @@
 using System.Reflection;
+using MassTransit.Logging;
 using MassTransit.Monitoring;
 using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 namespace BurgerLink.Shared.AppConfiguration;
 
 public static class OpenTelemetryConfigurationExtensions
 {
-    private static void ConfigureResource(ResourceBuilder obj, string serviceName)
-    {
-        obj.AddService(serviceName, serviceInstanceId: Environment.MachineName);
-    }
-
     public static void ConfigureTelemetry(this IServiceCollection serviceCollection)
     {
         var serviceName = Assembly.GetEntryAssembly().GetName().Name;
+        if (serviceName is null)
+        {
+            throw new ArgumentNullException(nameof(serviceCollection));
+        }
 
-        serviceCollection.AddOpenTelemetry().WithMetrics(opts => opts
+        serviceCollection.AddOpenTelemetry()
+            .ConfigureResource(builder => { builder.AddService(serviceName); })
+            .PrivateConfigureTracing()
+            .PrivateConfigureMetrics(serviceName);
+    }
+
+    private static OpenTelemetryBuilder PrivateConfigureMetrics(this OpenTelemetryBuilder openTelemetryBuilder,
+        string serviceName)
+    {
+        return openTelemetryBuilder.WithMetrics(opts => opts
             .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName))
             .AddMeter(InstrumentationOptions.MeterName)
             .AddAspNetCoreInstrumentation()
@@ -28,5 +39,19 @@ public static class OpenTelemetryConfigurationExtensions
             {
                 otlpExporterOptions.Endpoint = new Uri("http://otel-collector:4317/");
             }));
+    }
+
+    private static OpenTelemetryBuilder PrivateConfigureTracing(this OpenTelemetryBuilder openTelemetryBuilder)
+    {
+        return openTelemetryBuilder.WithTracing(tracerProviderBuilder =>
+        {
+            tracerProviderBuilder
+                .AddSource(DiagnosticHeaders.DefaultListenerName)
+                .AddConsoleExporter()
+                .AddOtlpExporter(otlpExporterOptions =>
+                {
+                    otlpExporterOptions.Endpoint = new Uri("http://otel-collector:4317/");
+                });
+        });
     }
 }
