409ad8c324fd614b04e3db4c7574e54ff76793a2
Shawn Neckelmann <shawn.neckelmann@nvoicepay.com>
2023-10-17 23:25:16 -0500

    wip

--- src/shared/BurgerLink.Shared/AppConfiguration/BurgerLinkConfigurationExtensions.cs
+++ src/shared/BurgerLink.Shared/AppConfiguration/BurgerLinkConfigurationExtensions.cs
@@ -35,23 +35,6 @@ public static class BurgerLinkConfigurationExtensions
         });
     }
 
-    public static void LoadAppSettings(this IConfigurationBuilder builder)
-    {
-        var path = Path.Combine(Directory.GetCurrentDirectory(), "settings", "commonsettings.json");
-
-        if (!File.Exists(path))
-        {
-            throw new FileNotFoundException("commonsettings.json not found.", path);
-        }
-
-        builder.Sources.Clear();
-        builder
-            .SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
-            .AddJsonFile(path, false, true);
-
-        builder.Build();
-    }
-
 
     public static void LoadAppSettings(this ConfigurationManager builder)
     {
