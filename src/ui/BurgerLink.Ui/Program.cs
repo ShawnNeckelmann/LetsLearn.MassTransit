6042c8a936e90ae48ade4c876706cf99c6d76d02
Shawn Neckelmann <shawn.neckelmann@nvoicepay.com>
2023-11-07 12:34:46 -0600

    wip

--- src/ui/BurgerLink.Ui/Program.cs
+++ src/ui/BurgerLink.Ui/Program.cs
@@ -6,21 +6,14 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
+    "default",
+    "{controller}/{action=Index}/{id?}");
 
 app.MapFallbackToFile("index.html");
 
