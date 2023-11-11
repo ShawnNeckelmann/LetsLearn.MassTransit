05064b8b3ff2328d77408c8a87f614e6df15271a
Shawn Neckelmann <shawn.neckelmann@nvoicepay.com>
2023-11-10 21:14:46 -0600

    wip

---
+++ src/ui/BurgerLink.Ui/Consumers/UpsertInventoryItemConsumer.cs
@@ -0,0 +1,13 @@
+﻿using BurgerLink.Inventory.Contracts.Commands;
+using MassTransit;
+
+namespace BurgerLink.Ui.Consumers;
+
+public class UpsertInventoryItemConsumer : IConsumer<UpsertInventoryItem>
+{
+    public Task Consume(ConsumeContext<UpsertInventoryItem> context)
+    {
+        Console.WriteLine(context.Message.ItemName);
+        return Task.CompletedTask;
+    }
+}
