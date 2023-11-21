812ca17a792ed52fc89098ed3cfa3bdc3fe6f258
Shawn Neckelmann <shawn.neckelmann@nvoicepay.com>
2023-11-21 12:46:04 -0600

    wip

--- src/ui/BurgerLink.Ui/Repository/InventoryMongoDbRepository.cs
+++ src/ui/BurgerLink.Ui/Repository/InventoryMongoDbRepository.cs
@@ -31,8 +31,19 @@ public class InventoryMongoDbRepository : BaseMongoCollection<InventoryItem>, II
             .Set(inventoryItem => inventoryItem.ItemName, item.ItemName)
             .Set(inventoryItem => inventoryItem.Quantity, item.Quantity);
 
-        await Collection.UpdateOneAsync(filter, update);
-        OnItemModified?.Invoke(this, item);
+
+        var options = new FindOneAndUpdateOptions<InventoryItem>
+        {
+            ReturnDocument = ReturnDocument.After
+        };
+
+        var updatedItem = await Collection.FindOneAndUpdateAsync(filter, update, options);
+        if (updatedItem == null)
+        {
+            return;
+        }
+
+        OnItemModified?.Invoke(this, updatedItem);
     }
 
     public event EventHandler<InventoryItem>? OnItemAdded;
